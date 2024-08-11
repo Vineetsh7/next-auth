3a529759c101ba079455d201e98c8615bcfad184
Vineetsh7 <119120435+Vineet1sh@users.noreply.github.com>
2024-08-11 21:24:03 +0530

    Server & Client page setup

---
+++ app/(protected)/server/page.tsx
@@ -0,0 +1,14 @@
+import { currentUser } from "@/lib/auth";
+import { UserInfo } from "@/components/user-info";
+
+const ServerPage = async () => {
+  const user = await currentUser();
+  return (
+    <UserInfo
+    user={user}
+    label="🌐Server components"
+    />  
+  );
+};
+
+export default ServerPage;
--- auth.ts
+++ auth.ts
@@ -50,6 +50,9 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       if (token.role && session.user) {
         session.user.role = token.role;
       }
+      if ( session.user) {
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+      }
       return session;
     },
     async jwt({ token }) {
@@ -58,6 +61,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       const existingUser = await getUserById(token.sub);
       if (!existingUser) return token;
       token.role = existingUser.role;
+      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
       return token;
     },
   },
